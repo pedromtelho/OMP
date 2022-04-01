a9b8461b6bfb6a1d8f81249f8fd55cb84eda1d20
Pedro Paulo Telho <pedromtelho@pc-198-184.customer.ask4.lan>
2022-04-01 15:15:18 +0100

    explaining concepts and adding examples

---
+++ data_sharing/private_var.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+
+int main()
+{
+    #pragma omp parallel {
+        int tmp = 0;
+        #pragma omp for private(tmp)
+        for (int i = 0; i < 100; i++) {
+            tmp += i;
+        }
+        printf("tmp: %d", tmp);
+    }
+     
+    
+    return 1;
+}
