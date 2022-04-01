8013e064fb4060b7c5ac81d3722510a0086c2c04
Pedro Paulo Telho <pedromtelho@pc-198-184.customer.ask4.lan>
2022-04-01 15:17:22 +0100

    fixing bug

--- data_sharing/private_var.cpp
+++ data_sharing/private_var.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <omp.h>
 
-
 int main()
 {
-    #pragma omp parallel {
-        int tmp = 0;
-        #pragma omp for private(tmp)
-        for (int i = 0; i < 100; i++) {
-            tmp += i;
-        }
-        printf("tmp: %d", tmp);
+    #pragma omp parallel
+    int tmp = 0;
+    #pragma omp for private(tmp)
+    for (int i = 0; i < 100; i++)
+    {
+        tmp += i;
     }
-     
-    
+    printf("tmp: %d", tmp);
+
     return 1;
 }
