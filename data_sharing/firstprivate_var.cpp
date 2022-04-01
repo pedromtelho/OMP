7c1d6dd6c803df18bc7b03f785ae3ca5de4485fd
Pedro Paulo Telho <pedromtelho@pc-198-184.customer.ask4.lan>
2022-04-01 15:21:24 +0100

    first private

--- data_sharing/firstprivate_var.cpp
+++ data_sharing/firstprivate_var.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <omp.h>
 
-// ESSE PROGRAMA DEVE DAR ERRO POIS VAI ASSUMIR QUE A VARIÁVEL tmp
-// NÃO FOI DECLARADA NO ESCOPO DO FOR LOOP
 
 int main()
 {
     #pragma omp parallel
     int tmp = 0;
-    #pragma omp for private(tmp)
+    #pragma omp for firstprivate(tmp)
     for (int i = 0; i < 100; i++)
     {
         tmp += i;
