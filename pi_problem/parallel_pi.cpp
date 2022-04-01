92c442bea084d9457840f87523834a16ca76f2df
pedomtelho <pedomtelho@hotmail.com>
2022-04-01 13:08:00 +0100

    fixing bugs

--- pi_problem/parallel_pi.cpp
+++ pi_problem/parallel_pi.cpp
@@ -8,13 +8,13 @@ int main()
     int i;
     double x, pi, sum = 0.0;
     step = 1.0 / (double)num_steps;
-    #pragma omp parallel for private(i, x) reduction(x: sum)
+    #pragma omp parallel for private(i, x) reduction(+:sum)
     for (i = 0; i < num_steps; i++)
     {
         x = (i + 0.5) * step;
         sum = sum + 4.0 / (1.0 + x * x);
     }
     pi = step * sum;
-    printf("PI VALUE: %d", pi);
+    printf("PI VALUE: %f \n", pi);
     return 1;
 }
