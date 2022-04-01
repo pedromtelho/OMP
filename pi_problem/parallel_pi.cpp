15c5e3573d0af430b2c02a5c1c93fdf81687ca07
Pedro Paulo Telho <pedromtelho@pc-198-184.customer.ask4.lan>
2022-04-01 13:10:25 +0100

    adding time verification

--- pi_problem/parallel_pi.cpp
+++ pi_problem/parallel_pi.cpp
@@ -6,6 +6,7 @@ double step;
 int main()
 {
     int i;
+    double time1 = omp_get_wtime();
     double x, pi, sum = 0.0;
     step = 1.0 / (double)num_steps;
     #pragma omp parallel for private(i, x) reduction(+:sum)
@@ -15,6 +16,8 @@ int main()
         sum = sum + 4.0 / (1.0 + x * x);
     }
     pi = step * sum;
+    double time2= omp_get_wtime() - time1;
     printf("PI VALUE: %f \n", pi);
+    printf("TIME: %f \n", time2);
     return 1;
 }
--- pi_problem/serial_pi.cpp
+++ pi_problem/serial_pi.cpp
@@ -6,6 +6,7 @@ double step;
 int main()
 {
     int i;
+    double time1 = omp_get_wtime();
     double x, pi, sum = 0.0;
     step = 1.0 / (double)num_steps;
     for (i = 0; i < num_steps; i++)
@@ -14,5 +15,8 @@ int main()
         sum = sum + 4.0 / (1.0 + x * x);
     }
     pi = step * sum;
+    double time2= omp_get_wtime() - time1;
+    printf("PI VALUE: %f \n", pi);
+    printf("TIME: %f \n", time2);
     return 1;
 }
